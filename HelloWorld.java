4891c996194d60962441281b8e35cf74a1ca0952
inteuniso <dzferry9@gmail.com>
2019-08-25 12:07:09 -0500

    Test of branching: interesting, need comments in order to commit.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld{
 public static void main(String[] args){
 
 //Print statement
-System.out.println("Hello World!");
+System.out.println("Hello World!" + "\nI'm learning GitHub.");
+
 }
 }
